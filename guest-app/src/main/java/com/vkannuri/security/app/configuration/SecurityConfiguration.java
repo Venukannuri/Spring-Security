074493336366f37a42208b329eed9349b48b6f20
Venu Kannuri <venugopal.kannuri@outlook.com>
2021-01-16 14:20:03 -0700

    Implemented basic authentication

---
+++ guest-app/src/main/java/com/vkannuri/security/app/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.vkannuri.security.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/** @author Venu Kannuri . */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/", "/index", "/css/*", "/js/*")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+}
