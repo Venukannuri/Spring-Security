1561e3b12c618e46889b790f49261e7f6dd24ab3
Venu Kannuri <venugopal.kannuri@outlook.com>
2021-01-16 16:47:59 -0700

    updated thymeleaf forms

--- guest-app/src/main/java/com/vkannuri/security/app/GuestAppController.java
+++ guest-app/src/main/java/com/vkannuri/security/app/GuestAppController.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
@@ -30,6 +34,11 @@ public class GuestAppController {
     public String getHomePage(Model model) {
 
         return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 
     @GetMapping(value = "/guests")
